c98e47f1d9f030f2dd903e36ac5d620735af30e2
Kimcheolhui <cjfgml0306@naver.com>
2025-05-19 19:19:04 +0900

    cached removed

--- SmartX-Mini-2025/Experiment/Lab-7. 3-Tier/frontend/deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env sh
-
-DOCKERHUB_ID=tori209
-DOCKERHUB_REPO=test-image
-IMAGE_TAG=latest
-
-sudo docker build . --tag ${DOCKERHUB_ID}/${DOCKERHUB_REPO}:${IMAGE_TAG} --no-cache &&
-sudo docker push ${DOCKERHUB_ID}/${DOCKERHUB_REPO}:${IMAGE_TAG} &&
-kubectl apply -f ../kubernetes/frontend
